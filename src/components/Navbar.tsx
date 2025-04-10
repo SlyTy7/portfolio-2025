691d0c630f5c578aa807626f846e22ac4370d42e
SlyTy7 <tyler.west.dev@gmail.com>
2025-04-10 14:48:02 -0700

    remove gutters and full-width

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,22 +2,58 @@
 
 // components/Navbar.jsx
 // MUI AppBar with links to Home, Projects, About, Contact
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import {
+	AppBar,
+	Toolbar,
+	Container,
+	Typography,
+	Button,
+	Box,
+} from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 export default function Navbar() {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component={RouterLink} to="/" sx={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}>
-          Tyler West
-        </Typography>
-        <Box>
-          <Button color="inherit" component={RouterLink} to="/projects">Projects</Button>
-          <Button color="inherit" component={RouterLink} to="/about">About</Button>
-          <Button color="inherit" component={RouterLink} to="/contact">Contact</Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
+	return (
+		<AppBar position="static" enableColorOnDark>
+			<Container maxWidth="lg" sx={{ mt: 1, mb: 1 }}>
+				<Toolbar disableGutters={true}>
+					<Typography
+						variant="h6"
+						component={RouterLink}
+						to="/"
+						sx={{
+							flexGrow: 1,
+							textDecoration: "none",
+							color: "inherit",
+						}}
+					>
+						Tyler West
+					</Typography>
+					<Box>
+						<Button
+							color="inherit"
+							component={RouterLink}
+							to="/projects"
+						>
+							Projects
+						</Button>
+						<Button
+							color="inherit"
+							component={RouterLink}
+							to="/about"
+						>
+							About
+						</Button>
+						<Button
+							color="inherit"
+							component={RouterLink}
+							to="/contact"
+						>
+							Contact
+						</Button>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
