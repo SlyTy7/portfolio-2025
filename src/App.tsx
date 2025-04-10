e874f821f19bbb68aa13cac87b2c39ee5376800c
SlyTy7 <tyler.west.dev@gmail.com>
2025-04-10 14:32:39 -0700

    add theme styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CssBaseline, Container, ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline, Container, Paper, ThemeProvider, createTheme } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -17,6 +17,9 @@ const darkTheme = createTheme({
     secondary: {
       main: '#651fff',
     },
+    background: {
+      default: '#121212',
+    },
   },
 });
 
@@ -25,16 +28,18 @@ export default function App() {
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Router>
-        <Navbar />
-        <Container className="appContainer" maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Container>
-        <Footer />
+        <Paper elevation={8}>
+          <Navbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Container>
+          <Footer />
+        </Paper>
       </Router>
     </ThemeProvider>
   );
