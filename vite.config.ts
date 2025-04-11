8924d0fdc261206d918ade64df06464eeff80488
SlyTy7 <tyler.west.dev@gmail.com>
2025-04-11 15:22:04 -0700

    add rollup-plugin-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'index.html', dest: 'dist', rename: '404.html' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
 })
