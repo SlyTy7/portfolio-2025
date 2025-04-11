5d7a30f947de162364606e3ce86aa39a0ca3e387
SlyTy7 <tyler.west.dev@gmail.com>
2025-04-11 10:52:21 -0700

    add avatar home button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,30 +6,32 @@ import {
 	AppBar,
 	Toolbar,
 	Container,
-	Typography,
+	Avatar,
+	IconButton,
+	Tooltip,
 	Button,
 	Box,
 } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
+import headshot from "../assets/tyler-west-headshot.jpg";
 
 export default function Navbar() {
 	return (
-		<AppBar position="static" enableColorOnDark>
+		<AppBar position="sticky" enableColorOnDark>
 			<Container maxWidth="lg" sx={{ mt: 1, mb: 1 }}>
 				<Toolbar disableGutters={true}>
-					<Typography
-						variant="h6"
-						component={RouterLink}
-						to="/"
-						sx={{
-							flexGrow: 1,
-							textDecoration: "none",
-							color: "inherit",
-						}}
-					>
-						Tyler West
-					</Typography>
-					<Box>
+					<Box sx={{ flexGrow: 1 }}>
+						<Tooltip title="Go Home">
+							<IconButton
+								component={RouterLink}
+								to="/"
+								sx={{ p: 0 }}
+							>
+								<Avatar alt="Tyler West" src={headshot} />
+							</IconButton>
+						</Tooltip>
+					</Box>
+					<Box sx={{}}>
 						<Button
 							color="inherit"
 							component={RouterLink}
