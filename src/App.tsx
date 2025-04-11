204dbb6dd33ba65468fcf0afe77d57bea7af7ead
SlyTy7 <tyler.west.dev@gmail.com>
2025-04-10 22:53:43 -0700

    change page containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,68 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CssBaseline, Container, ThemeProvider, createTheme } from "@mui/material";
+import {
+	CssBaseline,
+	Container,
+	ThemeProvider,
+	createTheme,
+} from "@mui/material";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import './App.css'
+import "./App.css";
 
 const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#00897b',
-    },
-    secondary: {
-      main: '#651fff',
-    },
-    background: {
-      default: '#121212',
-    },
-  },
+	palette: {
+		mode: "dark",
+		primary: {
+			main: "#00897b",
+		},
+		secondary: {
+			main: "#651fff",
+		},
+		background: {
+			default: "#121212",
+		},
+	},
 });
 
 export default function App() {
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Router>
-        <Navbar />
-        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Container>
-        <Footer />
-      </Router>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/projects"
+						element={
+							<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+								<Projects />
+							</Container>
+						}
+					/>
+					<Route
+						path="/about"
+						element={
+							<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+								<About />
+							</Container>
+						}
+					/>
+					<Route
+						path="/contact"
+						element={
+							<Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+								<Contact />
+							</Container>
+						}
+					/>
+				</Routes>
+				<Footer />
+			</Router>
+		</ThemeProvider>
+	);
 }
-
-
-
-
-
