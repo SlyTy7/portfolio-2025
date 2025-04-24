6bf2a7e2028678a9cc636a55e7eefda7eefadf03
SlyTy7 <tyler.west.dev@gmail.com>
2025-04-23 20:40:48 -0700

    add blog db

--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -1,7 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+const projectsFirebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
@@ -10,7 +10,24 @@ const firebaseConfig = {
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const blogFirebaseConfig = {
+    apiKey: import.meta.env.VITE_BLOG_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_BLOG_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_BLOG_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_BLOG_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_BLOG_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_BLOG_FIREBASE_APP_ID,
+}; 
 
-export { db };
+const projectsApp =
+  getApps().find((app) => app.name === "projects") ||
+  initializeApp(projectsFirebaseConfig, "projects");
+
+const blogApp =
+  getApps().find((app) => app.name === "blog") ||
+  initializeApp(blogFirebaseConfig, "blog");
+
+const db = getFirestore(projectsApp);
+const blogDb = getFirestore(blogApp);
+
+export { db, blogDb };
